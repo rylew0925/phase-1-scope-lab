96bf70bd0758c17dad235bb536d3cad796766356
Richard <rlew0925@gmail.com>
2024-12-07 22:07:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Declares a global variable
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; // Modifies the global variable
+}
+
+const leastFavoriteCustomer = 'angry piece of shit';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'you'; // This will throw an error
+}
